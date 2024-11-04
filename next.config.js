a27729be8eefa14bab29a2ea3296082e4ab519b3
todelusion <todelusion@gmail.com>
2024-11-05 00:47:28 +0800

    Remove unuse file

--- next.config.js
+++ next.config.js
@@ -5,18 +5,4 @@ module.exports = withTM({
   images: {
     unoptimized: true,
   },
-  async redirects() {
-    return [
-      {
-        source: "/home",
-        destination: "/",
-        permanent: true,
-      },
-      {
-        source: "/product-list",
-        destination: "/product-list/air",
-        permanent: true,
-      },
-    ];
-  },
 });
